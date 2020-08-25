a84834272db2b1e7a0ae3131f65e4b545951c738
Emile Fokkema <emilefokkema@gmail.com>
2020-08-25 18:50:03 +0200

    use babel to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+import babel from '@rollup/plugin-babel';
+
 export default {
 	input: 'src/index.js',
 	output: {
 		file: 'dist/index.js',
 		format: 'umd',
 		name: 'Esemantic'
-	}
+	},
+	plugins: [babel({ presets: ['@babel/preset-env'], babelHelpers: 'bundled' })]
 }
