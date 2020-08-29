5ff8e0cc7cb1a89df54272802ddb1e1f7cfb0ec8
Emile Fokkema <emilefokkema@gmail.com>
2020-08-29 16:31:48 +0200

    register notification when a visit is done

--- src/visit.js
+++ src/visit.js
@@ -23,13 +23,17 @@ class NodeWrapper{
 }
 
 var visit = function(node, visitor){
-	(function continuation(node, visitor, parentNode){
+	(function continuation(node, visitor){
 		var children = collection.getChildren(node);
 		for(var child of children){
 			var childWrapped = new NodeWrapper(child, node);
-			var newVisitor = collection.getNewVisitor(childWrapped, visitor);
+			var childVisitDoneHandlers = [];
+			var newVisitor = collection.getNewVisitor(childWrapped, visitor, (onDone) => {childVisitDoneHandlers.unshift(onDone);});
 			if(newVisitor){
-				continuation(childWrapped, newVisitor)
+				continuation(childWrapped, newVisitor);
+				for(var childVisitDoneHandler of childVisitDoneHandlers){
+					childVisitDoneHandler();
+				}
 			}
 		}
 	})(new NodeWrapper(node), visitor);
--- test/visit.test.js
+++ test/visit.test.js
@@ -199,3 +199,175 @@ describe('when replacing one visitor with another during a visit', () => {
 		expect(visitedVariableDeclaration).toBe(variableDeclaration);
 	});
 });
+
+describe('when a visit of a node is at an end', () => {
+	let tree, visitedChildrenOfFirstDeclarator, visitedChildrenOfSecondDeclarator;
+
+	beforeEach(() => {
+		// for script `var a = b + c;var d = e;`
+		tree = {
+			"type": "Program",
+			"start": 0,
+			"end": 26,
+			"body": [
+			  {
+				"type": "VariableDeclaration",
+				"start": 0,
+				"end": 14,
+				"declarations": [
+				  {
+					"type": "VariableDeclarator",
+					"start": 4,
+					"end": 13,
+					"id": {
+					  "type": "Identifier",
+					  "start": 4,
+					  "end": 5,
+					  "name": "a"
+					},
+					"init": {
+					  "type": "BinaryExpression",
+					  "start": 8,
+					  "end": 13,
+					  "left": {
+						"type": "Identifier",
+						"start": 8,
+						"end": 9,
+						"name": "b"
+					  },
+					  "operator": "+",
+					  "right": {
+						"type": "Identifier",
+						"start": 12,
+						"end": 13,
+						"name": "c"
+					  }
+					}
+				  }
+				],
+				"kind": "var"
+			  },
+			  {
+				"type": "VariableDeclaration",
+				"start": 15,
+				"end": 25,
+				"declarations": [
+				  {
+					"type": "VariableDeclarator",
+					"start": 19,
+					"end": 24,
+					"id": {
+					  "type": "Identifier",
+					  "start": 19,
+					  "end": 20,
+					  "name": "d"
+					},
+					"init": {
+					  "type": "Identifier",
+					  "start": 23,
+					  "end": 24,
+					  "name": "e"
+					}
+				  }
+				],
+				"kind": "var"
+			  }
+			],
+			"sourceType": "script"
+		  };
+		var visitedFirstDeclarator = false;
+		visit(tree, {
+			VariableDeclarator(node, useVisitor, onDone){
+				var childrenVisited = [];
+				onDone(() => {
+					if(!visitedFirstDeclarator){
+						visitedFirstDeclarator = true;
+						visitedChildrenOfFirstDeclarator = childrenVisited.slice();
+					}else{
+						visitedChildrenOfSecondDeclarator = childrenVisited.slice();
+					}
+				});
+				return {
+					Node(child){
+						childrenVisited.push(child);
+					}
+				};
+			}
+		});
+
+	});
+
+	it('the visit method should have been notified of that', () => {
+		expect(visitedChildrenOfFirstDeclarator).toEqual([
+			tree.body[0].declarations[0].id,
+			tree.body[0].declarations[0].init
+		]);
+		expect(visitedChildrenOfSecondDeclarator).toEqual([
+			tree.body[1].declarations[0].id,
+			tree.body[1].declarations[0].init
+		]);
+	});
+});
+
+describe('when registering a done handler in both the visitor method and the replacing visitor method', () => {
+	let tree, doneIndicators;
+
+	beforeEach(() => {
+		// for script `var a = b;`
+		tree = {
+			"type": "Program",
+			"start": 0,
+			"end": 11,
+			"body": [
+			  {
+				"type": "VariableDeclaration",
+				"start": 0,
+				"end": 10,
+				"declarations": [
+				  {
+					"type": "VariableDeclarator",
+					"start": 4,
+					"end": 9,
+					"id": {
+					  "type": "Identifier",
+					  "start": 4,
+					  "end": 5,
+					  "name": "a"
+					},
+					"init": {
+					  "type": "Identifier",
+					  "start": 8,
+					  "end": 9,
+					  "name": "b"
+					}
+				  }
+				],
+				"kind": "var"
+			  }
+			],
+			"sourceType": "script"
+		  };
+		doneIndicators = [];
+		visit(tree, {
+			Declaration(node1, useVisitor, onDone){
+				onDone(() => {
+					doneIndicators.push(1);
+				});
+				return useVisitor({
+					VariableDeclaration(node2, useVisitor, onDone){
+						onDone(() => {
+							doneIndicators.push(2);
+						});
+						return {
+							Node(){return this;}
+						};
+					}
+				});
+			}
+		});
+	});
+
+	it('they should have been called in the right order', () => {
+		expect(doneIndicators).toEqual([2, 1]);
+	});
+});
